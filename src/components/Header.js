927603ebb96caa22cf7f65c1ffcec4a16f4f927e
Sooyoung Yoon <ayoooo240@gmail.com>
2022-04-12 08:23:44 +0900

    💄 Header 컴포넌트 스타일링

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+
+import logo from "../images/icons/logo-sm.png";
+import cart from "../images/icons/cart-sm.png";
+import "./Header.scss"
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="nav-wrapper fixed-top navbar navbar-toggleable-sm navbar-expand-md">
+      <div className="container">
+        <Navbar className="w-100" collapseOnSelect expand="lg" variant="dark">
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Brand href="/">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="nav-justified w-100 nav-fill">
+              <Nav.Link href="/">iphone</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/user/cart">
+                <img src={cart} alt="cart" />
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
