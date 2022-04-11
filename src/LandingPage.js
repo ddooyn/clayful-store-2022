87721e9752dd953c01024f9f02f612532bb1c94e
Sooyoung Yoon <ayoooo240@gmail.com>
2022-04-11 21:20:01 +0900

    ✅ clayful 연동 여부 테스트

---
+++ src/LandingPage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import clayful from "clayful/client-js";
+
+function LandingPage() {
+  const Product = clayful.Product;
+
+  const options = {
+    query: {
+      page: 1,
+    },
+  };
+
+  Product.list(options, function (err, response) {
+    if (err) {
+      console.log(err.isClayful);
+    }
+
+    console.log(response.status);
+    console.log(response.headers);
+    console.log(response.data);
+  });
+  return <div>LandingPage 성공적?</div>;
+}
+
+export default LandingPage;
