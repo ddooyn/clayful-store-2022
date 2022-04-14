e2f7f96adf0953121e717c4cad2b2fe1883fca6d
Sooyoung Yoon <ayoooo240@gmail.com>
2022-04-14 21:52:58 +0900

    ✨ CartItem 컴포넌트 구현 💄 CartPage 스타일링 추가

--- src/pages/CartPage/Sections/CartItem.js
+++ src/pages/CartPage/Sections/CartItem.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
-function CartItem() {
+function CartItem({ item, index }) {
+  // if (!item.product) return null;
   return (
-    <div>CartItem</div>
-  )
+    <div className="item">
+      <div className="image">
+        <img
+          style={{ height: "100%" }}
+          src={item.product.thumbnail.url}
+          alt={item.product.name}
+        />
+      </div>
+
+      <div className="description">
+        <span>{item.product.name}</span>
+        <span>Bball High</span>
+        <span>White</span>
+      </div>
+
+      <div className="quantity">
+        <button type="button" name="button" className="minus-btn">
+          -
+        </button>
+        <input type="text" readOnly name="number" value={item.quantity.raw} />
+        <button type="button" name="button" className="plus-btn">
+          +
+        </button>
+      </div>
+
+      <div className="total-price">
+        ￦ {item.price.original.raw.toLocaleString()}
+      </div>
+
+      <div className="buttons">
+        <button type="button" className="delete-btn">X</button>
+      </div>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
