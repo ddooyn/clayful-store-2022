dd7007e421a2f4865e7074527b25b066a891903b
Sooyoung Yoon <ayoooo240@gmail.com>
2022-04-16 11:15:01 +0900

    🔨 StrictMode 해제 (렌더링 2번 이슈 해결하기 위해)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles/index.scss";
 import "./styles/auth.scss";
 import App from "./App";
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import clayful from "clayful/client-js";
@@ -18,9 +18,9 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
+  // </StrictMode>
 );
